840b6844b38dcbfebee26f82a85fd164b014a74f
tengdehong <657948468@qq.com>
2015-03-27 15:47:44 +0800

    第一次

---
+++ eLearn-core/src/main/java/org/tdh/eLearn/eLearn/model/App.java
@@ -0,0 +1,13 @@
+package org.tdh.eLearn.eLearn.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
